fdbf81e97227cda8a475dab25527b412683fedf1
Mark Richey <mkrichey@gmail.com>
2025-02-10 15:26:07 +0000

    Cookie Test

--- server.go
+++ server.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		var c, err = r.Cookie("cookieTest")
+		if err != nil {
+			fmt.Println("Nope")
+		} else {
+			fmt.Println(c.Value)
+		}
 		http.ServeFile(w, r, "static/about.html")
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:    "cookieTest",
+			Value:   "BooYa",
+			Expires: time.Now().Add(120 * time.Second),
+		})
 		http.ServeFile(w, r, "static/index.html")
 	})
 
